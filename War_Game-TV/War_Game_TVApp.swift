4a30f8e043a53af8bbf15c65186839bf5703e442
edenvvv <edenvvv@gmail.com>
2020-10-10 17:31:02 +0300

    Initial Commit

---
+++ War_Game-TV/War_Game_TVApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Game_TVApp.swift
+//  War_Game-TV
+//
+//  Created by User on 10/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct War_Game_TVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ War_Game-TVTests/War_Game_TVTests.swift
@@ -0,0 +1,33 @@
+//
+//  War_Game_TVTests.swift
+//  War_Game-TVTests
+//
+//  Created by User on 10/10/2020.
+//
+
+import XCTest
+@testable import War_Game_TV
+
+class War_Game_TVTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
